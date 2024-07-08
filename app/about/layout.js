04b427630cdf6de89f70c1d1da6f68849691741f
Mh-Monzil <monzil246@gmail.com>
2024-07-08 15:47:46 +0600

    nav added to about routing

--- app/about/layout.js
+++ app/about/layout.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
-const layout = ({children}) => {
-    return (
-        <div>
-            <nav>Mission | Vision</nav>
-            {children}
-        </div>
-    );
+const layout = ({ children }) => {
+  return (
+    <div>
+      <nav className="my-6">
+        <ul className="flex items-center gap-6 pb-10">
+          <li>
+            <Link href="/about/mission">Mission</Link>
+          </li>
+          <li>
+            <Link href="/about/vision">Vision</Link>
+          </li>
+        </ul>
+      </nav>
+      {children}
+    </div>
+  );
 };
 
 export default layout;
