65f0d37aac76af0250b9cf291f851999a93ea5f9
Mh-Monzil <monzil246@gmail.com>
2024-07-08 15:31:02 +0600

    about page added

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div className='mt-10'>
+            About Us Page
+        </div>
+    );
+};
+
+export default page;
--- app/page.js
+++ app/page.js
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-rose-500 font-bold text-6xl">hello world</h1>
+    <main className="mt-10">
+      <h1 className="text-rose-500 font-bold text-6xl ">hello world</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis cupiditate nemo ratione pariatur impedit qui mollitia tenetur numquam, ab alias.</p>
     </main>
   );
 }
